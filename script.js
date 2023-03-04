65474b71dcc07d0f07db24bdcadc0056ec603a7f
bui-brian <54672877+bui-brian@users.noreply.github.com>
2023-03-03 23:07:29 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const form = document.querySelector("#feedback-form");
+var radios = document.getElementsByName('rating');
+
+form.addEventListener("submit", function(e) {
+	e.preventDefault();
+    // alert("This is an alert!");
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            alert("Thank you for the " + radios[i].value + " star rating!");
+            break;
+        }
+    }
+});
